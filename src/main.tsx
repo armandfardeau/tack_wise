cf0a33a4f27d098b939125fab4e597ed3fbfe027
Armand Fardeau <armandfardeau@users.noreply.github.com>
2026-07-22 13:27:04 +0200

    Remove Featurebase SDK wiring (#70)

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
-import AppWithFeaturebase from './components/AppWithFeaturebase.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppWithFeaturebase />
+    <App />
     <Analytics />
     <SpeedInsights />
   </StrictMode>,
