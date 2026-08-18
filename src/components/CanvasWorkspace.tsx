dc9447c43d44e49a6384f959f4a7cecbcd635078
armandfardeau <fardeauarmand@gmail.com>
2026-08-18 20:41:36 +0200

    Adjust mobile inspector layout

--- src/components/CanvasWorkspace.tsx
+++ src/components/CanvasWorkspace.tsx
@@ -889,14 +889,6 @@ export default function CanvasWorkspace({
         />
       </div>
       {shouldShowRightInspector && (
-        <button
-          type="button"
-          className={inspectorStyles.rightInspectorBackdrop}
-          aria-label="Close inspector"
-          onClick={handleCloseInspector}
-        />
-      )}
-      {shouldShowRightInspector && (
         <aside className={inspectorStyles.rightInspectorPanel} aria-label="Object inspector">
           <div ref={inspectorRef} className={inspectorStyles.rightInspectorContent}>
             <Inspector
